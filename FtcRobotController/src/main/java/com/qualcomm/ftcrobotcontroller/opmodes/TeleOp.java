f8e176ed00bad00c47a6c200099536c6df004faf
derekhohos <derekhho@hotmail.com>
2015-11-10 19:44:40 -0600

    Testing arm

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
@@ -2,6 +2,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 
 /**
  * Created by Lev on 27.10.2015.
@@ -15,26 +16,30 @@ public class TeleOp extends OpMode {
 
     @Override
     public void init() {
-        leftMotor = hardwareMap.dcMotor.get("left_drive");
+        /*leftMotor = hardwareMap.dcMotor.get("left_drive");
         rightMotor = hardwareMap.dcMotor.get("right_drive");
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        */
         armPosition = hardwareMap.dcMotor.get("arm_position");
+
+        // armPosition.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
     }
 
     @Override
     public void loop() {
-        leftY = -gamepad1.left_stick_y;
+        /* leftY = -gamepad1.left_stick_y;
         rightY = -gamepad1.right_stick_y;
         leftMotor.setPower(leftY);
         rightMotor.setPower(rightY);
 
-        if (gamepad2.dpad_up)
+*/
+        if (gamepad2.dpad_up && armPosition.getCurrentPosition() <= 360)
         {
-            armPosition.setPower(0.25);
+            armPosition.setPower(0.2);
         }
-        else if (gamepad2.dpad_down)
+        else if (gamepad2.dpad_down && armPosition.getCurrentPosition() >= 0)
         {
-            armPosition.setPower(-0.25);
+            armPosition.setPower(-0.2);
         }
         else {
             armPosition.setPower(0);
